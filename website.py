8bc103845a751a04b3f903ea227d714ce336a0a4
Tianxj22 <3456053429@qq.com>
2024-06-01 20:50:34 +0800

    这个项目可以自动把图片上传到图床网站， 并生成链接

---
+++ website.py
@@ -0,0 +1,6 @@
+#————————————————————————————————记录所有url网址——————————————————————————————————————————#
+
+
+PROFILE_URL = 'https://sm.ms/api/v2/profile'            # 获取用户信息的网址
+HISTORY_URL = 'https://sm.ms/api/v2/upload_history'     # 获取所有提交历史记录
+UPLOAD_URL  = 'https://sm.ms/api/v2/upload'             # 上传图片的历史记录
